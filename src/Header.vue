7aa1b30199093c5c9fe34d59faadf1ab5c9a826a
Shav <shavenchew@yahoo.com>
2017-01-24 21:02:34 -0800

    Added a Vue Header (nav) component and Materialize CDNs

---
+++ src/Header.vue
@@ -0,0 +1,18 @@
+<template id="">
+
+  <nav>
+  <div class="nav-wrapper">
+    <a href="#" class="brand-logo left">Portfolio Page</a>
+    <ul id="nav-mobile" class="right hide-on-med-and-down">
+      <li><a href="#a">Home</a></li>
+      <li><a href="#a">About</a></li>
+      <li><a href="#a">Portfolio</a></li>
+      <li><a href="#a">Contact Me</a></li>
+    </ul>
+  </div>
+</nav>
+</template>
+
+<script type="text/javascript">
+
+</script>
