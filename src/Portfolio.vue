2afe6601f63048c990f06def3342637cc9fcc17c
Shav <shavenchew@yahoo.com>
2017-04-03 23:36:32 -0700

    Added a list of techs used on Projects

--- src/Portfolio.vue
+++ src/Portfolio.vue
@@ -28,7 +28,8 @@
 
         <div class="col m6 s12" v-for='project in projects'>
           <div class='port-item1'>
-            <h5>{{ project.name }}</h5>
+            <h4>{{ project.name }}</h3>
+            <h6> {{ project.techs }} </h5>
             <img v-bind:src=project.image>
             <a v-bind:href='project.githubLink'>Github</a><span> |</span>
             <a v-bind:href='project.liveLink'>Live Example</a>
@@ -56,44 +57,54 @@
             image: require('./assets/StockChart.png'),
             githubLink: 'https://github.com/Bigghead/fcc-stock-market',
             liveLink: 'https://cryptic-fjord-45243.herokuapp.com/',
-            static: false
+            static: false,
+            techs: ['HTML', 'Materialize', 'Node/Express', 'Highcharts']
           },
           {
             name: "Voting App",
             image: require('./assets/fccVoting.png'),
             githubLink: 'https://github.com/Bigghead/fccVotingApp',
             liveLink: 'https://fcc-voting-app-ver2.herokuapp.com/',
-            static: false
+            static: false,
+            techs: ['HTML', 'Bootstrap', 'Node/Express', 'Chart.JS', 'EJS']
           },
           {
             name: "Night-Life Coordination",
             image: require('./assets/NightLife.png'),
             githubLink: 'https://github.com/Bigghead/fcc-nightlife',
             liveLink: 'https://calm-shelf-79440.herokuapp.com/',
-            static: false
+            static: false,
+            techs: ['HTML', 'Bootstrap', 'Node/Express', 'Google Maps API', 'YELP API', 'EJS']
+
           },
           {
             name: "Book Trading App",
             image: require('./assets/BookTradingApp.png'),
             githubLink: 'https://github.com/Bigghead/book-trading-app',
-            liveLink: 'https://lychee-pie-43109.herokuapp.com/',            
-            static: true
+            liveLink: 'https://lychee-pie-43109.herokuapp.com/',
+            static: true,
+            techs: ['HTML', 'Materialize', 'Node/Express', 'EJS']
+
           },
           {
             name: "Momentum Clone",
             image: require('./assets/Momentum.png'),
             githubLink: 'https://github.com/Bigghead/Momentum-Clone',
-            liveLink: 'http://rawgit.com/Bigghead/Momentum-Clone/master/index.html',            
-            static: true
+            liveLink: 'http://rawgit.com/Bigghead/Momentum-Clone/master/index.html',
+            static: true,
+            techs: ['HTML', 'CSS', 'JavaScript']
+
           },
           {
             name: "Camper Leaderboard",
             image: require('./assets/CamperLeaderboard.png'),
             githubLink: 'https://github.com/Bigghead/Camper-Leaderboard',
-            liveLink: 'http://exultant-bit.surge.sh/',            
-            static: true
+            liveLink: 'http://exultant-bit.surge.sh/',
+            static: true,
+            techs: ['HTML', 'React.JS', 'Bootstrap']
+
           }
-          
+
         ]
       }
     }
@@ -101,11 +112,12 @@
 
 </script>
 
-<style media="screen">
+<style media="screen" scoped>
   img {
     width: 100%;
     box-shadow: 0 0 8px rgba(0, 0, 0, 0.5);
     border: solid 2px grey;
+    margin-top: 5%;
   }
   
   .port-item {
