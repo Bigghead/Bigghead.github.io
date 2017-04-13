4504fad75f654cb9ab5a5877c2150bca25cd92a7
Shav <shavenchew@yahoo.com>
2017-04-13 14:24:53 -0700

    Redoing webpack build to push latest to heroku

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,10 @@
 </template>
 
 <script>
-
 import Header from './Header.vue'
 import FirstPage from './FirstPage.vue'
 import About from './About.vue'
 import Portfolio from './Portfolio.vue'
-
 import TestAbout from './Test.vue';
 export default {
   components:{
@@ -22,13 +20,9 @@ export default {
     'port-page': Portfolio
   }
 }
-
-
-
 </script>
 
 <style>
-
 body{
   font-family: 'Lobster';
 }
@@ -39,26 +33,21 @@ body{
   text-align: center;
   color: #2c3e50;
 }
-
 h1, h2 {
   font-weight: normal;
 }
-
 ul {
   list-style-type: none;
   padding: 0;
 }
-
 li {
   display: inline-block;
   margin: 0 10px;
 }
-
 a {
   color: #42b983;
   font-family: 'Avenir';
 }
-
 p{
   color:#00FFFF;
  font-size:2em;
