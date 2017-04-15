1f775b37f4709d5cc23fbeaadbdae2e0ed8ff9b5
Shav <shavenchew@yahoo.com>
2017-04-14 18:05:30 -0700

    pulicPath webpack changed to './dist/' for prod

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
+    publicPath: './dist/',
     filename: 'build.js'
   },
   module: {
@@ -40,7 +40,8 @@ module.exports = {
   },
   resolve: {
     alias: {
-      'vue$': 'vue/dist/vue.common.js'
+      'vue$': 'vue/dist/vue.common.js',
+      'vue': 'vue/dist/vue.js'
     }
   },
   devServer: {
