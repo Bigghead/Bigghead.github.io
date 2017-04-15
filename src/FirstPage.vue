abd97837271744058fa9a5e450849462084803ae
Shav <shavenchew@yahoo.com>
2017-04-14 22:12:23 -0700

    using animate css on name. Making 1st page 100vh. Stable dev. Need to fix ScrollReveal

--- src/FirstPage.vue
+++ src/FirstPage.vue
@@ -1,17 +1,19 @@
 <template id="">
-  <div class ="pageone center-align">
-
-       <div class='box '>
+  <div class="pageone center-align">
+    <div class="wrapper animated rollIn">
+      <div class='box '>
         <h1>Shav D. Parta</h1>
         <h4>Story Of A Noob</h4>
-        
 
-       <div class='btn-list'>
-        <a class ='waves-effect waves-light btn' href='https://github.com/Bigghead'>Github</a>
-        <a class ="waves-effect waves-light btn" href='https://codepen.io/Bigghead/pens/public/'>Codepen</a>
-       </div>
-     </div>
-   </div>
+
+        <div class='btn-list'>
+          <a class='waves-effect waves-light btn' href='https://github.com/Bigghead'>Github</a>
+          <a class="waves-effect waves-light btn" href='https://codepen.io/Bigghead/pens/public/'>Codepen</a>
+        </div>
+      </div>
+
+    </div>
+  </div>
 </template>
 
 <script type="text/javascript">
@@ -19,24 +21,28 @@
 </script>
 
 <style media="screen">
-.pageone{
-  background :url("https://images.unsplash.com/photo-1475198751283-d3f5d231c7f4?dpr=1&amp;auto=format&amp;fit=crop&amp;w=1500&amp;h=1000&amp;q=80&amp;cs=tinysrgb&amp;crop=");
-  background-size: cover;
-  height: 100vh;
-  background-attachment:fixed;
-  background-position:fixed;
+  .pageone {
+    background: url("https://images.unsplash.com/photo-1475198751283-d3f5d231c7f4?dpr=1&amp;auto=format&amp;fit=crop&amp;w=1500&amp;h=1000&amp;q=80&amp;cs=tinysrgb&amp;crop=");
+    background-size: cover;
+    height: 100vh;
+    background-attachment: fixed;
+    background-position: fixed;
     position: relative;
+  }
 
-}
-.box{
-   position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  color: white;
-}
-
-/*@media screen 
+  .wrapper{
+    width: 100%;
+    height: 100%;
+  }
+  
+  .box {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%) !important;
+    color: white;
+  }
+  /*@media screen 
   and (min-device-width: 1200px) 
   and (max-device-width: 1600px) 
   and (-webkit-min-device-pixel-ratio: 1) { 
@@ -44,5 +50,4 @@
       height: 770px;
     }
 }*/
-
 </style>
