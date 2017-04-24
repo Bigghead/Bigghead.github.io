3c3a36681ef5db3fb33411e53c7f49188dcc2e14
Shav <shavenchew@yahoo.com>
2017-04-24 14:45:02 -0700

    fixed + mobile navbar

--- src/Header.vue
+++ src/Header.vue
@@ -1,8 +1,11 @@
 <template id="">
+<div class="navbar-fixed">
 
   <nav>
     <div class="nav-wrapper">
+      <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
       <div class="brand-logo center">
+
         <ul id="nav-mobile" class="center-align hide-on-small-only">
           <li><a href="#a">Home</a></li>
           <li><a href="#about-me">About</a></li>
@@ -10,9 +13,15 @@
           <li><a href="#contact-me">Contact Me</a></li>
         </ul>
       </div>
-
+         <ul class="side-nav" id="mobile-demo">
+          <li><a href="#a">Home</a></li>
+          <li><a href="#about-me">About</a></li>
+          <li><a href="#portfolio">Portfolio</a></li>
+          <li><a href="#contact-me">Contact Me</a></li>
+        </ul>
     </div>
   </nav>
+</div>
 </template>
 
 <script type="text/javascript">
