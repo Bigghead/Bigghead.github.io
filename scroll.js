ff764e2bea5aa6fbef7a30919e0d499887fc1e87
Bigghead <shavenchew@yahoo.com>
2018-04-25 16:29:33 -0700

    cleaning folder structure

--- scroll.js
+++
@@ -1,8 +0,0 @@
-window.sr = ScrollReveal();
-        sr.reveal('.box', { duration: 2000, viewFactor: 0.3 }, 100);
-
-    sr.reveal('.port-item1', { duration: 2000, viewFactor: 0.3 }, 100);
-    sr.reveal('.port-item2', { duration: 2000, viewFactor: 0.3 }, 100);
-    sr.reveal('.port-item3', { duration: 2000, viewFactor: 0.3 }, 100);
-    sr.reveal('.port-item4', { duration: 2000, viewFactor: 0.3 }, 100);
-    sr.reveal('.profilePic', { duration: 2000, viewFactor: 1 }, 100);
